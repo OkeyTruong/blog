544171188f7cce5e709ecc5d1e2bb5a61e1cd83a
huy <okey222k@gmail.com>
2022-02-03 00:54:38 +0700

    design home page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../components/header/Header";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./home.css"
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div className="home">
+          <Sidebar/>
+      </div>
+    </>
+  );
+}
