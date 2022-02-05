54ee5cf0c89e218f93bbb24229ddd4af5f22ba6e
huy <okey222k@gmail.com>
2022-02-05 07:34:50 +0700

    mongoose connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const app = express();
 
-app.listen("5000",()=>{
-    console.log("z");
-})
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+app.listen("5000", () => {
+  console.log("Backend is running...");
+});
