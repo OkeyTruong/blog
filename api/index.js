d3258a0c6dec036e0b0b3d45a47d151eca9072a1
huy <okey222k@gmail.com>
2022-02-08 11:14:10 +0700

    login & register

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth")
 
 const app = express();
+app.use(express.json())
 
 dotenv.config();
 
@@ -14,6 +16,8 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
+app.use("/api/auth",authRoute)
+
 app.listen("5000",()=>{
     console.log("Backend is running...");
-})
+}) 
