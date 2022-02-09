b1e66b5c2a1bde8f9bc839b062db1f28b80db08d
huy <okey222k@gmail.com>
2022-02-09 13:22:29 +0700

    Register System

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,29 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import "./register.css";
 
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(false);
+    try {
+      const res = await axios.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+      res.data && window.location.replace("/login");
+    } catch (error) {
+      setError(true);
+      toast.error('Something went wrong', {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        });
+    }
+  };
+
   return (
     <div className="register">
       <span className="registerTitle">register</span>
-      <form className="registerForm">
+      <form className="registerForm" onSubmit={handleSubmit}>
         <label>username</label>
         <input
           type="text"
           className="registerInput"
           placeholder="Enter your username..."
+          onChange={(e) => setUsername(e.target.value)}
         />
         <label>email</label>
         <input
           type="email"
           className="registerInput"
           placeholder="Enter your email..."
+          onChange={(e) => setEmail(e.target.value)}
         />
         <label>password</label>
         <input
           type="password"
           className="registerInput"
           placeholder="Enter your password..."
+          onChange={(e) => setPassword(e.target.value)}
         />
         <button className="registerButton" type="submit">
           register
@@ -34,6 +70,9 @@ export default function Register() {
           login
         </Link>
       </button>
+      {error && (
+        <ToastContainer className="error"/>
+      )}
     </div>
   );
 }
