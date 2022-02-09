ebc3b874656de15b8c0ea707d900256e87bf251d
huy <okey222k@gmail.com>
2022-02-09 16:18:47 +0700

    Login System

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useContext, useRef } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
+import axios from "axios";
 import "./login.css";
 
 export default function Login() {
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { dispatch, isFecthing } = useContext(Context);
+
+  const handleSubmit = async  (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("/auth/login", {
+        username: userRef.current.value,
+        password: passwordRef.current.value,
+      });
+      dispatch({ type: "LOGIN_SUCCESS",payload:res.data });
+    } catch (error) {
+      dispatch({ type: "LOGIN_SUCCESS" });
+    }
+  };
   return (
     <div className="login">
       <span className="loginTitle">login</span>
-      <form className="loginForm">
+      <form className="loginForm" onSubmit={handleSubmit}>
         <label>username</label>
         <input
           type="text"
           className="loginInput"
           placeholder="Enter your username..."
+          ref={userRef}
         />
         <label>password</label>
         <input
           type="password"
           className="loginInput"
           placeholder="Enter your password..."
+          ref={passwordRef}
         />
         <button className="loginButton" type="submit">
           Login
